107b001f6544e67d2ebafe395617967711814044
muskan70 <muskanmangla70@gmail.com>
2024-06-08 12:31:07 +0530

    updates

--- FactoryDesignPattern/polygon.go
+++ FactoryDesignPattern/polygon.go
@@ -152,19 +152,3 @@ func (o *Octagon) GetFeatures() {
 func (o *Octagon) GetParameter() float64 {
 	return 8 * float64(o.sideLen[0])
 }
-
-type Circle struct {
-	radius int
-}
-
-func (c *Circle) GetName() string {
-	return "CIRCLE"
-}
-
-func (c *Circle) GetFeatures() {
-	fmt.Println("It is a circle of radius:", c.radius)
-}
-
-func (c *Circle) GetParameter() float64 {
-	return 6.28 * float64(c.radius)
-}
